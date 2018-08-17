cce41d41ec7004cf7aff9040dae11a242af3958d
Yo Yehudi <yochannah@gmail.com>
2018-08-17 15:24:58 +0100

    update guidance and build methods to be more robust

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Cymine from "cytoscape-network";
+import Cymine from "@intermine/cytoscape-intermine";
 
 //make sure to export main, with the signature
 export function main(el, service, imEntity, state, config) {
