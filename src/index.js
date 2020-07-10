974d9f8082587895850b554777132104668fc4c7
uosl <regen@anche.no>
2020-07-10 17:04:14 +0100

    Support tool API v2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Cymine from "@intermine/cytoscape-intermine";
 
 //make sure to export main, with the signature
-export function main(el, service, imEntity, state, config) {
+export function main(el, service, entity, state, config) {
+  var imEntity = entity.Gene || entity.Protein;
 
   var paths = {"Gene" : "Gene.id", "Protein" : "Gene.proteins.id"},
       initOptions = {
