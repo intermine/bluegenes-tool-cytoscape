4bd99be1f90f28b9e685dca7150f3d325dc19334
uosl <regen@anche.no>
2021-04-19 15:42:49 +0100

    Fix styling by making consistent with newer Bluegenes Tools

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import Cymine from "@intermine/cytoscape-intermine";
 export function main(el, service, entity, state, config) {
   var imEntity = entity.Gene || entity.Protein;
 
+  // We attach to this element to fix the CSS, as cytoscape-intermine expects
+  // `el` to be inside the element with the CSS class, while they are both the
+  // same element in modern Bluegenes tools.
+  var innerElem = document.createElement('div');
+  el.appendChild(innerElem);
+
   var paths = {"Gene" : "Gene.id", "Protein" : "Gene.proteins.id"},
       initOptions = {
-    parentElem: el,
+    parentElem: innerElem,
     service: service,
     queryOn: {
       "value": imEntity.value,
